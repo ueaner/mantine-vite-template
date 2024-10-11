5dc0abacdc19d9bb4bcd3a021ceec390d8132e41
ueaner <ueaner@gmail.com>
2024-10-11 19:21:21 +0800

    build: migrate yarn/npm to pnpm and `@storybook/testing-library` to `@storybook/test`

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -24,3 +24,4 @@ export const decorators = [
   (renderStory: any) => <ColorSchemeWrapper>{renderStory()}</ColorSchemeWrapper>,
   (renderStory: any) => <MantineProvider theme={theme}>{renderStory()}</MantineProvider>,
 ];
+export const tags = ['autodocs'];
